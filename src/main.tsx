fb4ba6db978013dd6efbcc1289e1d39ecd7579b3
Marcus Sandi <tradedomarcus@gmail.com>
2023-01-26 14:53:26 -0300

    feat: add react rounter and dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { RecoilRoot } from 'recoil'
 import AppRoutes from './routes'
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AppRoutes/>
-    </BrowserRouter>
+    <RecoilRoot>
+      <BrowserRouter>
+        <AppRoutes/>
+      </BrowserRouter>
+    </RecoilRoot>
   </React.StrictMode>,
 )
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,15 @@
 import { Route, Routes } from "react-router-dom"
 
 // pages
-import Auth from "./pages/Auth/Auth"
+import Auth from "./pages/Auth/auth"
+import Home from "./pages/Home/home"
 import Login from "./pages/Login/login"
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Login/>}/>
+      <Route path="/home" element={<Home/>}/>
       <Route path="/auth" element={<Auth/>} >
         <Route path=":token" />
       </Route>
