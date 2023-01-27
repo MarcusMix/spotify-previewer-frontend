c2dfb9a94a534a15c68543ae6db06910877d3bee
Marcus Sandi <tradedomarcus@gmail.com>
2023-01-27 08:42:55 -0300

    chore: add tailwindcss

---
+++ tailwind.config.cjs
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  purge: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: 'Roboto'
+      },
+      colors: {
+        "spotify-green": "#1DB954",
+        "spotify-green-200" : "#11913f"
+      }
+    },
+  },
+  plugins: [],
+}
