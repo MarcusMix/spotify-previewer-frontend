fa4967e2dca3431e15ed96ad426087fea7583715
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-07 20:25:39 -0300

    feat: add selector getSearch

--- src/store/selector/selector.ts
+++ src/store/selector/selector.ts
@@ -1,6 +1,7 @@
 import {selector} from 'recoil'
 import { requester } from '../../api/requester'
-import { atomToken } from '../atoms/atom'
+import { TSpotifySearch } from '../../interfaces/interface'
+import { atomSearch, atomToken } from '../atoms/atom'
 
 export const selectorGetUser = selector({
   key: 'selectorGetUser',
@@ -12,6 +13,22 @@ export const selectorGetUser = selector({
     const {data} = await requester({
       Authorization: token,
     }).get('/me')
+
+    return data
+  }
+})
+
+export const selectorGetSearch = selector({
+  key: 'selectorGetSearch',
+  get: async ({get}): Promise<TSpotifySearch | undefined> => {
+    const token = get(atomToken)
+    const search = get(atomSearch)
+
+    if(!token || !search) return
+
+    const {data} = await requester({
+      Authorization: token,
+    }).get(`search?q=${search}&type=track%2Cartist%2Calbum&market=BR&limit=35`)
 
     return data
   }
